a24a74499d5dd56fd248ffe6b864d1d9022f29f2
AnsM <ansonmau172@gmail.com>
2025-08-25 03:44:05 -0400

    refactor: reorganizes agent functions into helper classes

--- Board.py
+++ Board.py
@@ -5,13 +5,21 @@ class Board:
                 self.numRows = numRows
                 self.numCols = numCols
                 self.prevMove = ()
-                self.points = 0
+                self.point_count = 0
                 self.turnNumber = 0
-                self.board = self._generateBoard()
-                pass
+                self.board_raw = self.generateBoard()
+                
+                self.place = place(self)
+                self.isValid = isValid(self)
+                self.clear = clear(self)
+                self.clearUtils = clearUtils(self)
+                self.utils = boardUtils(self)
+                self.points = points(self)
+                self.shapeUtils = shapeUtils(self)
         
-        def getEmptyBoard(self):
-                return Board(self.numRows, self.numCols)
+        def play(self, shape: Shape, pos):
+                self.place.shape(shape, pos)
+
 
         def getTurnNumber(self):
                 return self.turnNumber
@@ -19,7 +27,10 @@ class Board:
         def getSize(self):
                 return (self.numRows, self.numCols)
         
-        def _generateBoard(self):
+        def getBoard(self):
+                return self.board
+        
+        def generateBoard(self):
                 board = []
                 for _ in range(self.numRows):
                         currRow = []
@@ -28,183 +39,204 @@ class Board:
                         board.append(currRow)
                 return board 
         
-        def getBoard(self):
-                return self.board
-        
+
         def reset(self):
-                self.board = self._generateBoard()
-                self.lastPlacedShape = None
+                self.board = self.generateBoard()
                 self.points = 0
                 self.turnNumber = 0
         
+
+
+class boardUtils:
+        def __init__(self, board: Board):
+                self.board = board
+
         def printBoard(self):
-                for col in range(self.numCols + 2):
+                for _ in range(self.board.numCols + 2):
                         print("-", end = '   ')
                 print()
 
-                for row in self.board:
+                for row in self.board.getBoard():
                         print("|", end = '   ')
                         for unit in row:
                                 print("X" if unit else ' ', end = '   ')
                         print("|")
 
-                for col in range(self.numCols + 2):
+                for _ in range(self.board.numCols + 2):
                         print("-", end = '   ')
                 print()
-                return
+                return 
+
+class shapeUtils:
+        def __init__(self, board: Board):
+                self.board = board
         
-        def placeShape(self, shape: Shape, pos):
-                if self.canPlaceShape(shape, pos):
-                        for position in self.getShapeCoords(shape, pos):
-                                self.place(position)
-                        self.prevMove = (shape, pos)
-                        self.checkClear()
-                        self.turnNumber += 1
-                        return True
+        def getBlockPositions(shape: Shape, pos):
+                positions = []
+                target_row, target_col = pos
+
+                for dR, dC in shape.getOffsets():
+                        positions.append(target_row + dR, target_col + dC)
                 
-                return False
+                return positions
 
+class place:
+        def __init__(self, board: Board):
+                self.board = board
+        
+        def _activate(self, pos):
+                row,col = pos
+                self.board.board_raw[row][col] = True
+        
+        def block(self, pos):
+                assert self.board.isValid.block(pos)
+                self._activate(pos)
 
-        def place(self, pos):
+        def shape(self, shape, pos):
+                assert self.board.isValid.shape(shape, pos)
+
+                for curr_position in self.board.shapeUtils.getBlockPositions(shape, pos):
+                        self._activate(curr_position)
+
+                
+class isValid:
+        def __init__(self, board: Board):
+                self.board = board
+        
+        def _isEmpty(self, pos):
                 row, col = pos
-                if self.canPlace(pos):
-                        self.board[row][col] = True
-                        return True
-                return False
-
-                # try:
-                #         self.board[row][col] = True
-                #         return True
-                # except:
-                #         return False
+                return self.board.board_raw[row][col] == False
         
-
-        
-        def checkClear(self):
-                cleared = 0
-                colsCleared = []
-                rowsCleared = []
-
-                for row, col in self.getShapeCoords(*self.prevMove):
-                        if (col not in colsCleared) and self._checkColClear(col):
-                                cleared += 1
-                                colsCleared.append(col)
-                        if (row not in rowsCleared) and self._checkRowClear(row):
-                                cleared += 1
-                                rowsCleared.append(row)
-                
-                if cleared > 0:
-                        for col in colsCleared:
-                                self._clearCol(col)
-                        for row in rowsCleared:
-                                self._clearRow(row)
-
-                        self.addPoints(100 + 200*(cleared-1))
-                        
-                return
-        
-        def potentialPoints(self, shape: Shape, pos):
-                cleared = 0
-                colsCleared = []
-                rowsCleared = []
-                points = 0
-
-                shapeCoords = self.getShapeCoords(shape, pos)
-
-                for row,col in shapeCoords:
-                        if (col not in colsCleared) and self._checkColClearGhost(col, shapeCoords):
-                                cleared += 1
-                                colsCleared.append(col)
-                        if (row not in rowsCleared) and self._checkRowClearGhost(row, shapeCoords):
-                                cleared += 1
-                                rowsCleared.append(row)
-                
-                if cleared > 0:
-                        points += 100 + 200*(cleared-1)
-
-                return points 
-
-        def _checkColClear(self, col):
-                for i in range(self.numRows):
-                        if not self.board[i][col]:
-                                return False
-                return True
-
-        def _checkColClearGhost(self, col, shapeCoords):
-                for i in range(self.numRows):
-                        currPos = (i, col)
-                        if not self.board[i][col] and currPos not in shapeCoords:
-                                return False
-                return True
-
-        def _checkRowClear(self, row):
-                for i in range(self.numCols):
-                        if not self.board[row][i]:
-                                return False
-                return True
-        
-        def _checkRowClearGhost(self, row, shapeCoords):
-                for i in range(self.numCols):
-                        currPos = (row, i)
-                        if not self.board[row][i] and currPos not in shapeCoords:
-                                return False
-                return True
-        
-        def _clearCol(self, col):
-                for i in range(self.numRows):
-                        self.board[i][col] = False
-                return
-        
-        def _clearRow(self, row):
-                for i in range(self.numCols):
-                        self.board[row][i] = False
-                return
-
-        def addPoints(self, pointsGained):
-                self.points += pointsGained
-        
-        def getPoints(self):
-                return self.points
-        
-        def canPlace(self, pos):
+        def block(self, pos):
                 row, col = pos
-                if (0 <= row < self.numRows) and (0 <= col < self.numCols):
-                        if not self.board[row][col]:
+                if (0 <= row < self.board.numRows) and (0 <= col < self.board.numCols):
+                        if self._isEmpty(pos):
                                 return True
                 return False
 
-        def canPlaceShape(self, shape: Shape, pos):
+        def shape(self, shape: Shape, pos):
                 row, col = pos
-                shapeH, shapeW = shape.getDims()
+                shape_height, shape_width = shape.getDims()
 
-                if (row+shapeH) > self.numRows - 1 or (col+shapeW) > self.numCols - 1:
+                # check if shape would even fit on the board at pos
+                if (row + shape_height) > self.board.numRows - 1 or (col + shape_width) > self.board.numCols - 1:
                         return False
 
-                for currPosition in self.getShapeCoords(shape, pos):
-                        if not self.canPlace(currPosition):
+                # check if any positions are already filled
+                for currPosition in self.board.shapeUtils.getBlockPositions(shape, pos):
+                        if not self._isEmpty(currPosition):
                                 return False
                         
                 return True
-
-        def getPossiblePositions(self, shape: Shape):
+        
+        def getValidPositions(self, shape: Shape):
                 possiblePositions = []
-                for row in range(self.numRows):
-                        for col in range(self.numCols):
+                for row in range(self.board.numRows):
+                        for col in range(self.board.numCols):
                                 currPos = (row, col)
-                                if self.canPlaceShape(shape, currPos):
+                                if self.shape(shape, currPos):
                                         possiblePositions.append(currPos)
 
                 return possiblePositions
 
-        def getShapeCoords(self, shape: Shape, pos):
-                row,col = pos
-                coords = []
-                offsets = shape.getOffsets()
+class clear:
+        def __init__(self, board: Board):
+                self.board = board
 
-                for dr, dc in offsets:
-                        coords.append((row+dr, col+dc))
-                
-                return coords
+        def check(self, last_played_move):
+                cleared = 0
+                colsCleared = []
+                rowsCleared = []
 
+                shape, pos = last_played_move
+
+                blocks_pos = self.board.shapeUtils.getBlockPositions(shape, pos)
+                for row, col in blocks_pos:
+                        if (col not in colsCleared) and self.board.clearUtils.isColFull(col):
+                                colsCleared.append(col)
+                        if (row not in rowsCleared) and self.board.clearUtils.isRowFull(row):
+                                rowsCleared.append(row)
+
+                total_cleared = len(colsCleared) + len(rowsCleared)
                 
+                if total_cleared > 0:
+                        points = (100 + 200*(cleared-1))
+                else:
+                        points = 0
+                        
+                return points
         
+        def checkWithMask(self, shape, pos):
+                colsCleared = []
+                rowsCleared = []
+                
+                blocks_pos = self.board.shapeUtils.getBlockPositions(shape, pos)
+                for row, col in blocks_pos:
+                        if col not in colsCleared and self.board.clearUtils.isColFull_withMask(col, blocks_pos):
+                                colsCleared.append(col)
+                        if row not in rowsCleared and self.board.clearUtils.isRowFull_withMask(row, blocks_pos):
+                                rowsCleared.append(row)
+                
+                total_cleared = len(colsCleared) + len(rowsCleared)
+                if total_cleared > 0:
+                        points = 100 + 200*(total_cleared-1)
+                else:
+                        points = 0
+
+                return points
+        
+        def clearCol(self, col):
+                for i in range(self.board.numRows):
+                        self.board[i][col] = False
+                return
+        
+        def clearRow(self, row):
+                for i in range(self.board.numCols):
+                        self.board[row][i] = False
+                return
+
+class clearUtils:
+        def __init__(self, board: Board):
+                self.board = board
+
+        def isColFull(self, col):
+                for i in range(self.board.numRows):
+                        pos = (i,col)
+                        if not self._checkPosition(pos):
+                                return False
+                return True
+
+        def isColFull_withMask(self, col, shapeCoords):
+                for i in range(self.board.numRows):
+                        currPos = (i, col)
+                        if not self._checkPosition(currPos) and currPos not in shapeCoords:
+                                return False
+                return True
+
+        def isRowFull(self, row):
+                for i in range(self.board.numCols):
+                        pos = (row,i)
+                        if not self._checkPosition(pos):
+                                return False
+                return True
+        
+        def isRowFull_withMask(self, row, shapeCoords):
+                for i in range(self.numCols):
+                        currPos = (row, i)
+                        if not self._checkPosition(currPos) and currPos not in shapeCoords:
+                                return False
+                return True
+
+        def _checkPosition(self, pos):
+                row,col = pos
+                return self.board.board_raw[row][col]
+
+class points:
+        def __init__(self, board: Board):
+                self.board = board
+
+        def add(self, pointsGained):
+                self.board.points += pointsGained
+        
+        def get(self):
+                return self.board.points
