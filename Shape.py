e71d97fca9af56ea3cd1e28ca703c70be59a91ac
AnsM <ansonmau172@gmail.com>
2025-08-25 03:45:06 -0400

    feat: change shape creation to be id by default

--- Shape.py
+++ Shape.py
@@ -1,3 +1,5 @@
+SHAPENAMES = ['single','small TR corner','small TL corner','small BL corner','small BR corner','large TR corner','large TL corner','large BL corner','large BR corner','small square','large square','2 horizontal','3 horizontal','4 horizontal','5 horizontal','2 vertical','3 vertical','4 vertical','5 vertical']
+
 SHAPE_PATTERNS = {
         0: '1000000000000000000000000',
         1: '1100001000000000000000000',
@@ -20,120 +22,72 @@ SHAPE_PATTERNS = {
         18: '1000010000100001000010000'
 }
 
-SHAPENAMES = ['single','small TR corner','small TL corner','small BL corner','small BR corner','large TR corner','large TL corner','large BL corner','large BR corner','small square','large square','2 horizontal','3 horizontal','4 horizontal','5 horizontal','2 vertical','3 vertical','4 vertical','5 vertical']
+shape_offsets = {}
+shape_dims = {}
 
-SHAPENAME_TO_ID = {
-        'single': 0,
-        'small TR corner': 1,
-        'small TL corner': 2,
-        'small BL corner': 3,
-        'small BR corner': 4,
-        'large TR corner': 5,
-        'large TL corner': 6,
-        'large BL corner': 7,
-        'large BR corner': 8,
-        'small square': 9,
-        'large square': 10,
-        '2 horizontal': 11,
-        '3 horizontal': 12,
-        '4 horizontal': 13,
-        '5 horizontal': 14,
-        '2 vertical': 15,
-        '3 vertical': 16,
-        '4 vertical': 17,
-        '5 vertical': 18
-}
+def initialize_shape_data():
+        for shape_id in SHAPE_PATTERNS:
+                shape_offsets[shape_id] = getOffsetsFromID(id)
+                shape_dims[shape_id] = getDimsFromID(id)
+
+def getDimsFromID(shapeID):
+        spriteHeight = 5
+        spriteWidth = 5
+
+        width = 0
+        height = 0
+
+        pattern = SHAPE_PATTERNS[shapeID]
+
+        for row in range(spriteHeight):
+                for col in range(spriteWidth):
+                        ind = row*5 + col
+                        if pattern[ind] == '1':
+                                height = row
+                                if col > width:
+                                        width = col
+                                
+        return (height+1, width+1)
+
+def getOffsetsFromID(shapeID):
+        offsets = []
+        pattern = SHAPE_PATTERNS[shapeID]
+        for rowCount in range(5):
+                for colCount in range(5):
+                        ind = rowCount*5 + colCount
+                        if pattern[ind] == '1':
+                                offsets.append((rowCount, colCount))
+        
+        return offsets
 
 class Shape:
-        def __init__(self, name):
-                self.name = name
-                self.id = SHAPENAME_TO_ID[name]
+        def __init__(self, id):
+                self.name = SHAPENAMES[id]
+                self.id = id
                 self.pattern = SHAPE_PATTERNS[self.id]
-
-                self.offsets = []
-                self.dims = ()
-                
-                self._initDims()
-                self._initOffsets()
         
-        def createFromID(ID):
-                shapeName = SHAPENAMES[ID]
-                return Shape(shapeName)
+        @staticmethod
+        def createFromName(name):
+                id = SHAPENAMES.index(name)
+                return Shape(id)
 
         def __str__(self):
                 return self.name
         
         def getOffsets(self):
-                return self.offsets
+                return shape_offsets[self.id]
 
         def getDims(self):
-                return self.dims
+                return shape_dims[self.id]
 
         def getID(self):
                 return self.id
 
+        def getPattern(self):
+                return SHAPE_PATTERNS[self.id]
+
         def clone(self):
-                newShape = Shape(self.name)
-                return newShape
-        
-        def getPos(self):
-                return self.position
-        
-        def getName(self):
-                return self.name
-        
-        def _initOffsets(self):
-                self.offsets = []
-                for rowCount in range(5):
-                        for colCount in range(5):
-                                ind = rowCount*5 + colCount
-                                if self.pattern[ind] == '1':
-                                        self.offsets.append((rowCount, colCount))
+                return Shape(self.id)
                 
-        def _initDims(self):
-                spriteHeight = 5
-                spriteWidth = 5
-
-                width = 0
-                height = 0
-
-                for row in range(spriteHeight):
-                        for col in range(spriteWidth):
-                                ind = row*5 + col
-                                if self.pattern[ind] == '1':
-                                        height = row
-                                        if col > width:
-                                                width = col
-                                        
-                self.dims = (height+1, width+1)
         
 
-        def getDimsFromID(shapeID):
-                spriteHeight = 5
-                spriteWidth = 5
-
-                width = 0
-                height = 0
-
-                pattern = SHAPE_PATTERNS[shapeID]
-
-                for row in range(spriteHeight):
-                        for col in range(spriteWidth):
-                                ind = row*5 + col
-                                if pattern[ind] == '1':
-                                        height = row
-                                        if col > width:
-                                                width = col
-                                        
-                return (height+1, width+1)
-
-        def getOffsetsFromID(shapeID):
-                offsets = []
-                pattern = SHAPE_PATTERNS[shapeID]
-                for rowCount in range(5):
-                        for colCount in range(5):
-                                ind = rowCount*5 + colCount
-                                if pattern[ind] == '1':
-                                        offsets.append((rowCount, colCount))
-                
-                return offsets
