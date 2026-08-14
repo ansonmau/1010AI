acb77157c6d6c79289c480a996c1b34f383ea84d
ansonmau <ansonmau172@gmail.com>
2026-08-13 20:10:55 -0400

    feat: adds tensor calculations

--- src/QNet/Agent.py
+++ src/QNet/Agent.py
@@ -9,10 +9,19 @@ if TYPE_CHECKING:
 
 class Agent:                
     def __init__(self, board: "Board"):
-        self._board             = board
-        self._inventory            = []
-        self._gai = GlobalActionIndex()
+        self._board         = board
+        self._inventory     = []
 
-        self._tensor_board = torch.tensor(self._board.get_board(), dtype=torch.float32)
+        self.gai            = GlobalActionIndex()
+        self.inventory_size = 3
+
+
+    def get_inventory_tensor(self):
+        inv_tensor = []
+        for shape in self._inventory:
+            inv_tensor.append(torch.tensor(shape.get_arr_repr(), dtype=torch.float32))
+        return torch.stack(inv_tensor) # should be (3, 5, 5)
+
+    def get_board_tensor(self):
+        return torch.tensor(self._board.get_board(), dtype=torch.float32) # (1, 10, 10)
         
-    def _generate_inventory_tensor
