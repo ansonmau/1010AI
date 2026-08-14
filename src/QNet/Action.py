ea6bdb181a99d2303b8df09344459e0224e214c3
ansonmau <ansonmau172@gmail.com>
2026-08-14 12:19:20 -0400

    deletes unused files'

--- src/QNet/Action.py
+++
@@ -1,7 +0,0 @@
-from Game.Shape import Shape
-
-class Action:
-        def __init__(self, shape: Shape, row, col):
-                self.shape = shape
-                self.pos = (row, col)
-                
