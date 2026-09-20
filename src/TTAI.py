c94cbd1173a4ce0a6305378791edcd0c5daa22b3
ansonmau <ansonmau172@gmail.com>
2026-09-20 16:19:41 -0400

    Checkpoints added and small changes to code for readability
    feat: adds checkpoint saving functionality
    refactor: turns checking episode count into a function
    refactor: reduce code by passing a dict instead of a variable holding the dict

--- src/TTAI.py
+++ src/TTAI.py
@@ -1,49 +1,70 @@
 # ──────────────────────────────────────────────────────────────────────
-from collections import deque
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     pass
 # ──────────────────────────────────────────────────────────────────────
+import os
 import random
 import torch 
-from QNet.Agent import Agent, EPSILON_DECAY, EPSILON_MIN
+from QNet.Agent       import Agent, EPSILON_DECAY, EPSILON_MIN
+from QNet.Checkpoint  import Checkpoint
 from Game.Board.Board import Board
-from Ui.stats import StatTrak
+from Ui.stats         import StatTrak
 # ──────────────────────────────────────────────────────────────────────
 
 AVERAGES_WINDOW_SIZE = 100
 
 class TTAI:
     def __init__(self):
+        self.episode_count = 100000
+        self.batch_size    = 256
+        self.greedy_freq   = 100
+        self.epsilon       = 1
+        self.save_freq     = 50
+        self.upload_freq   = 100
+
         self.board = Board(10,10)
         self.agent = Agent(self.board)
-
-        # self.agent._reward_calculator.enable_print_rewards()
-
-        self.episode_count = 10000
-        self.epsilon       = 1
-        self.batch_size    = 128
-        self.greedy_freq   = 100
-        self.st            = StatTrak(self.episode_count, AVERAGES_WINDOW_SIZE)
+        self.st    = StatTrak(self.episode_count, AVERAGES_WINDOW_SIZE)
+        self.chk = Checkpoint()
 
 
     def run(self):
         for curr_episode in range(self.episode_count):
+            def on(freq):
+                return curr_episode % freq == 0
+
             self.agent.reset()
             self.st.new_episode()
 
-            greedy = (curr_episode % self.greedy_freq == 0)
-            cEps = 0 if greedy else self.epsilon
+            if on(self.greedy_freq):
+                cEps = 0
+            else:
+                cEps = self.epsilon
 
-            sesh_data = {
-                    "type": "session",
-                    "epsilon": cEps,
-                    }
-            self.st.update(sesh_data)
+            if on(self.save_freq):
+                qs, os = self.agent.get_save_states()
+                self.chk.update({
+                    "qnet_state": qs,
+                    "optimizer_state": os,
+                    "stats": self.st.get_save_data(),
+                    "ep": curr_episode,
+                    })
+                self.chk.save("9")
+            
+            if on(self.upload_freq):
+                self.chk.upload()
 
+            self.st.update({
+                "type": "session",
+                "epsilon": cEps,
+                })
+
+
+            x = cEps==0
             while self.agent._can_play():
-                # ── agent loop ────────────────────────────────────────────────────────
+                # ── agent loop ────────────────────────────────────────────────────────nlucky sequenc
                 state = self.agent._observe_gamestate()
                 move = self.agent.choose_move(cEps)
                 next_state, reward, can_play, info = self.agent.step(move)
@@ -51,10 +72,11 @@ class TTAI:
                 self.agent.training_step(self.batch_size)
                 self.agent.update_target_net() 
 
-                # ── update stats ──────────────────────────────────────────────────────
+                # ── update stats ──────────────────────────────────────────────────────nlucky sequenc
                 ep_data = {
                         "type":             "episode",
                         "reward":           reward,
+                        "reward_list":      info["reward_list"],
                         "can_play":         can_play,
                         "move":             move,
                         "board_fill_ratio": self.board.utils.get_filled_ratio(),
